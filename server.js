42294a96dfd5e872dfb1ddb1e8c0e589022951b0
kitpoom <poom14.kt@gmail.com>
2026-02-27 15:36:34 +0700

    added: Authentication

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const dotenv = require('dotenv');
+// cookie to store token
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+// prevent injection
+const expressMongoSanitize = require('@exortek/express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+
+// Rate limit
+const rateLimit = require('express-rate-limit');
+
+// query
+const hpp = require('hpp');
+
+// Cors
+const cors = require('cors');
+
+//route file
+// const hospitals = require('./routes/hospitals');
+// const appointment = require('./routes/appointments');
+const auth = require('./routes/auth');
+
+//load env
+dotenv.config({path:'./config/config.env'});
+
+//connect to db
+connectDB();
+
+const app = express();
+//body parser
+app.use(express.json());
+//cookie parser
+app.use(cookieParser());
+
+// Enable cors
+app.use(cors());
+
+// Sanitize data
+app.use(expressMongoSanitize());
+// app.use(xss());
+
+// Set security helmet
+app.use(helmet());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10*60*1000, // 10 min
+    max: 100
+});
+
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp());
+
+
+//mount router
+// app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
+// app.use('/api/v1/appointments', appointment);
+
+
+// set parser for pagination
+app.set('query parser', 'extended');
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, 
+    console.log('Server running in ', 
+        process.env.NODE_ENV, 
+        ' mode on port ', PORT)
+    );
+
+//handle unhandled promise rejectiom
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //close server && exit port
+    server.close(() => process.exit(1));
+})
